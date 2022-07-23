9b940013a1317025c37e0e06268979f46a87b150
abarbatei <alin.barbatei@gmail.com>
2022-07-23 17:49:06 +0300

    added relevat pydocs and types where applicable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import json
 import asyncio
+from typing import List
+from web3.types import LogReceipt
 
 from web3environment import Web3Interface, Blockchains
 from utils import get_logger
@@ -32,12 +34,22 @@ class EventScraper:
         self.publisher = RabbitPublisher(config)
 
     @staticmethod
-    def load_events_filter():
+    def load_events_filter() -> dict:
+        """
+        Reads the content of INPUT_FILE_NAME (json file) and returns it. Simple helper function
+        @return: the targeted contract events and filters
+        """
         with open(EventScraper.INPUT_FILE_NAME, "rt") as fin:
             return json.load(fin)
 
     def setup_filters(self):
-
+        """
+        Function is the entry point to the main logic of the code.
+        All business logic related code is executed from here.
+        It sets up an asyncio coroutine for each smart contract filter and monitors it.
+            On each event will publish the event data on a previously created RabbitMQ server
+        @return: nothing
+        """
         loop = asyncio.get_event_loop()
 
         for contract_data in self.targeted_events_data['contracts']:
@@ -82,7 +94,21 @@ class EventScraper:
             loop.close()
 
     @staticmethod
-    def compose_filter_creation_execution_string(event_name, argument_filters):
+    def compose_filter_creation_execution_string(event_name, argument_filters) -> str:
+        """
+        Will combine the given parameters to form a python execution string for setting up a new filter.
+        This will be removed, but for the purpose of this POC, it will stay and be properly documented.
+
+        Example:
+        event_name: "PairCreated"
+        argument_filters: {"fromBlock": "latest"}
+        return: 'contract.events.PairCreated.createFilter(fromBlock="latest")'
+
+        @param event_name: the contract event name on which the filter will be added
+        @param argument_filters: the "filter_params" that will be passed to createFilter function, as
+        defined in https://web3py.readthedocs.io/en/stable/web3.eth.html#filters
+        @return: the string composing the function to be executed
+        """
         normalised_args = ""
         for argument, value in argument_filters.items():
             try:
@@ -95,6 +121,10 @@ class EventScraper:
         return function_string
 
     async def filter_loop(self, event_filter, polling_interval, event_name, filter_arguments):
+        """
+        Entry point for the async functions. Passes arguments to handle_event function and polls polling_interval
+        seconds for new events to pass
+        """
         self.logger.info("Starting asyncio filter routine {} {} with arguments: {}".format(
             event_name, event_filter, filter_arguments))
         while True:
@@ -102,7 +132,15 @@ class EventScraper:
                 self.handle_event(event_data, filter_arguments, event_name)
             await asyncio.sleep(polling_interval)
 
-    def handle_event(self, event, filter_arguments, event_name):
+    def handle_event(self, event: List[LogReceipt], filter_arguments: dict, event_name: str):
+        """
+        Function handles the received event from the smart contract, the filter arguments used and the event name.
+        Will group the data and publish it on the indicated RabbitMQ routing key
+        @param event: the blockchain event as returned by the API
+        @param filter_arguments: the dict with the filter parameters used
+        @param event_name: the Event name
+        @return: nothing, it publishes to the message broker new events
+        """
         data = {
             "event_name": event_name,
             "filter_arguments": filter_arguments,
